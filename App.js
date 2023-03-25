2c9bef5b55dae9fceec06aebc8ba3ff9777aaacb
freitasxandrei <freitasxandrei@gmail.com>
2023-03-25 18:19:24 -0300

    correcaoCores

--- App.js
+++ App.js
@@ -73,12 +73,12 @@ export default function App() {
 
         {buttons.map((button) => 
           button === '=' ? // Mapeamento do botão =
-        <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, {backgroundColor: '#3dd0e3'}]}>
+        <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, {backgroundColor: '#1e1240'}]}>
           <Text style={[styles.textButton, {color: "white", fontSize: 30}]}>{button}</Text>
         </TouchableOpacity>
           : // Mapeamento dos outros botões
           <TouchableOpacity onPress={() => handleInput(button)} key={button} style={styles.button}>
-            <Text style={[styles.textButton, {color: typeof(button) === 'number' ? 'black': '#0093a6'}]}>{button}</Text>
+            <Text style={[styles.textButton, {color: typeof(button) === 'number' ? 'white': 'grey'}]}>{button}</Text>
           </TouchableOpacity>
         )}
       </View>
@@ -94,10 +94,10 @@ const styles = StyleSheet.create({
   results: {
     flex: 2,
     justifyContent: "center",
-    backgroundColor: "#f5f5f5"
+    backgroundColor: "#1e1240"
   },
   resultText: {
-    color: "#282F38",
+    color: "#fffdff",
     fontSize: 32,
     fontWeight: "bold",
     padding: 12,
@@ -114,7 +114,7 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
   },
   button: {
-    backgroundColor: 'white',
+    backgroundColor: '#3d0075',
     alignItems: 'center',
     justifyContent: 'center',
     minWidth: 90, 
@@ -124,5 +124,6 @@ const styles = StyleSheet.create({
   textButton: {
     color: "#7c7c7c",
     fontSize: 20,
+    color: "white",
   } 
 });
